2daf3c5401d2c3d7a7baf45bd0b0e9384b5502b5
Emil Håkansson <emil.hakansson@jgy.se>
2022-05-12 15:52:29 +0200

    Hardmode (WIP)

--- Assets/GameMechanics/Scripts/Hard Mode/HardMode.cs
+++ Assets/GameMechanics/Scripts/Hard Mode/HardMode.cs
@@ -7,51 +7,67 @@ public class HardMode : MonoBehaviour
 {
 
 
-    public GameObject HardModeButtons;
-    public GameObject HardModeParent;
-
+    public GameObject HardModeButton;
+    public GameObject Canvas;
 
     GameObject newButton;
 
     Vector3 screenCenter;
     Vector3 SpawnPoint;
 
+    float[] speed = new float[5];
+
+    GameObject[] buttons = new GameObject[5];
+
     bool moving = true;
     // Start is called before the first frame update
     void Start()
     {
-        screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
-        SpawnPoint = new Vector3(-300f, 200f, 0);
 
-        spawn();
+
+        screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+
+        for (int i = 0; i < 5; i++)
+        {
+            speed[i] = Random.Range(0.1f, 1f);
+            SpawnPoint = new Vector3(Screen.width, Random.Range(0f, 800f), 0);
+            buttons[i] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
+            buttons[i].transform.SetParent(Canvas.transform);
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moving == true)
-        {
-            HardModeParent.transform.GetChild(0).position = Vector3.MoveTowards(HardModeButtons.transform.position, screenCenter, 0.5f);
-        }
+            for (int i = 0; i < 5; i++)
+            {
+                buttons[i].transform.position = Vector3.MoveTowards(buttons[i].transform.position, screenCenter, speed[i]);
+            
+                if (buttons[i].transform.position == screenCenter)
+            {
+                Destroy(buttons[i]);
+                spawn(i);
+            }
 
-        if (HardModeParent.transform.GetChild(0).position == screenCenter)
-        {
-            HardModeLoss();
-        }
+            }
+
+       
+     
     }
 
-    public void HardModeLoss()
+    public void Destroy()
     {
-        moving = false;
         Destroy(newButton);
-        Destroy(HardModeButtons);
     }
 
-    public void spawn()
+    public void spawn(int CurrentButton)
     {
-        newButton = Instantiate(HardModeButtons) as GameObject;
-        newButton.transform.SetParent(HardModeParent.transform);
-        newButton.transform.position = SpawnPoint;
+        speed[CurrentButton] = Random.Range(0.1f, 1f);
+        SpawnPoint = new Vector3(Screen.width, Random.Range(0f, 800f), 0);
+        buttons[CurrentButton] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
+        buttons[CurrentButton].transform.SetParent(Canvas.transform);
+
+
     }
 }
