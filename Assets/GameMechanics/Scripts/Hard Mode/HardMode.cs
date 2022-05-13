fbb063bb17c9a3164fa3551d8c4e5cfdb9001ef7
Emil Håkansson <emil.hakansson@jgy.se>
2022-05-13 09:49:03 +0200

    hardmode WIP

--- Assets/GameMechanics/Scripts/Hard Mode/HardMode.cs
+++ Assets/GameMechanics/Scripts/Hard Mode/HardMode.cs
@@ -15,9 +15,10 @@ public class HardMode : MonoBehaviour
     Vector3 screenCenter;
     Vector3 SpawnPoint;
 
-    float[] speed = new float[5];
+    float[] middle = new float[10];
+    float[] speed = new float[10];
 
-    GameObject[] buttons = new GameObject[5];
+    GameObject[] buttons = new GameObject[10];
 
     bool moving = true;
     // Start is called before the first frame update
@@ -25,23 +26,34 @@ public class HardMode : MonoBehaviour
     {
 
 
-        screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+
 
         for (int i = 0; i < 5; i++)
         {
+            middle[i] = Random.Range(Screen.height - Screen.height + Screen.height/3, Screen.height - Screen.height/3);
             speed[i] = Random.Range(0.1f, 1f);
             SpawnPoint = new Vector3(Screen.width, Random.Range(0f, 800f), 0);
             buttons[i] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
             buttons[i].transform.SetParent(Canvas.transform);
         }
+        for (int i = 5; i < 10; i++)
+        {
+            middle[i] = Random.Range(Screen.height - Screen.height + Screen.height / 3, Screen.height - Screen.height / 3);
+            speed[i] = Random.Range(0.1f, 1f);
+            SpawnPoint = new Vector3(Screen.width - Screen.width, Random.Range(0f, 800f), 0);
+            buttons[i] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
+            buttons[i].transform.SetParent(Canvas.transform);
+        }
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
+                screenCenter = new Vector3(Screen.width * 0.5f, middle[i], 0);
                 buttons[i].transform.position = Vector3.MoveTowards(buttons[i].transform.position, screenCenter, speed[i]);
             
                 if (buttons[i].transform.position == screenCenter)
@@ -63,10 +75,22 @@ public class HardMode : MonoBehaviour
 
     public void spawn(int CurrentButton)
     {
-        speed[CurrentButton] = Random.Range(0.1f, 1f);
-        SpawnPoint = new Vector3(Screen.width, Random.Range(0f, 800f), 0);
-        buttons[CurrentButton] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
-        buttons[CurrentButton].transform.SetParent(Canvas.transform);
+        if (CurrentButton <= 4)
+        {
+            speed[CurrentButton] = Random.Range(0.1f, 1f);
+            SpawnPoint = new Vector3(Screen.width, Random.Range(0f, 800f), 0);
+            buttons[CurrentButton] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
+            buttons[CurrentButton].transform.SetParent(Canvas.transform);
+        }
+
+        if (CurrentButton >= 5)
+        {
+            speed[CurrentButton] = Random.Range(0.1f, 1f);
+            SpawnPoint = new Vector3(Screen.width - Screen.width, Random.Range(0f, 800f), 0);
+            buttons[CurrentButton] = Instantiate(HardModeButton, SpawnPoint, Quaternion.identity);
+            buttons[CurrentButton].transform.SetParent(Canvas.transform);
+        }
+
 
 
     }
